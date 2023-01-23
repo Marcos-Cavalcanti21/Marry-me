1f2089729cd8861a8accc00ebc9caf9b48a6bd4a
Marcos Cavalcanti <72262936+Marcos-Cavalcanti21@users.noreply.github.com>
2023-01-23 15:35:32 -0300

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,16 @@
 function sim() {
 
-    setTimeout(() => {
-        document.getElementsByClassName('content')[0].src = './public/assets/fireworks.gif';
-    }, 500);
-
     alert("Ela disse Sim!!!! s2 s2")
 
     /* window.location.href = "/public/pages/fireworks/fireworks.html" */
-    document.getElementById('sim').style.margin = `${Math.random}vmax, ${Math.random}vmax`
+    /* document.getElementById('sim').style.margin = `${Math.random}vmax, ${Math.random}vmax` */
 }
 
-function recusa(){
-    debugger
+function recusa() {
     let x = parseInt(Math.random() * visualViewport.width),
-    y = parseInt(Math.random() * visualViewport.height);
+        y = parseInt(Math.random() * visualViewport.height);
 
-    console.log(x,y)
+    console.log(x, y)
 
-    document.getElementById('nao').style.margin = `${y}px ${x}px`
+    document.getElementsByClassName('nao')[0].style.margin = `${y}px ${x}px`
 }
